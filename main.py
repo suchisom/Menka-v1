91565de6347de34308dcba9213ddb0bed57b3a0a
suchi <master8008blaster@gmail.com>
2026-01-20 18:58:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.prompts import PromptTemplate
 from langchain_core.documents import Document
 from langchain.chains.question_answering import load_qa_chain
+#from langchain.chains.question_answering import load_qa_chain
 from pinecone import Pinecone, ServerlessSpec
 from langchain_pinecone import PineconeVectorStore
 
